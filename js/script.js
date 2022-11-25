a82fa86fbe306300b4924df8f34712c7da832925
Tomasz Pernal <tompern1@googlemail.com>
2022-11-25 09:14:23 +0000

    playerMove added

---
+++ js/script.js
@@ -0,0 +1,6 @@
+var computerMove;
+computerMove = 'kamień';
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+var playerMove;
+playerMove = 'papier';
+printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to kamień, to przegrywasz!');
